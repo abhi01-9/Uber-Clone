154e8030f3ca3fb5c82d6a3cce3897174b832168
Abhi Bhaskar <abhibhaskarnav2002@gmail.com>
2024-12-19 00:38:18 +0530

    login user authentication done

--- Backend/app.js
+++ Backend/app.js
@@ -1,15 +1,27 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 const express = require("express");
-const cors = require('cors');
+const cors = require("cors");
 const app = express();
+const cookieParser = require("cookie-parser");
+const connectToDb = require("./db/db");
+const userRoutes = require("./routes/user.routes");
+
+connectToDb();
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-
-app.get('/',(req,res)=>{
-    res.send("hellow world");
+app.get("/", (req, res) => {
+  res.send("hellow world");
 });
 
+app.use("/users", userRoutes);
+
+app.use("/temp", (req, res) => {
+  res.send("hii");
+});
 
 module.exports = app;
