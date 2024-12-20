30da22830a53da18f683aec3ef659ff10b3c9d30
Abhi Bhaskar <abhibhaskarnav2002@gmail.com>
2024-12-20 20:59:32 +0530

    Register Captain done

--- Backend/app.js
+++ Backend/app.js
@@ -6,6 +6,7 @@ const app = express();
 const cookieParser = require("cookie-parser");
 const connectToDb = require("./db/db");
 const userRoutes = require("./routes/user.routes");
+const captainRoutes = require("./routes/captain.routes");
 
 connectToDb();
 
@@ -18,10 +19,8 @@ app.get("/", (req, res) => {
   res.send("hellow world");
 });
 
-app.use("/users", userRoutes);
+app.use('/users', userRoutes);
+app.use('/captains',captainRoutes);
 
-app.use("/temp", (req, res) => {
-  res.send("hii");
-});
 
 module.exports = app;
